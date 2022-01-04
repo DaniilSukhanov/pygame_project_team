6cd4ed99712c4bcbb81083ea81be287bd5ff4f63
DaniilSukhanov <danchopancho@icloud.com>
2022-01-05 01:38:28 +0300

    1. Реализовано загрузки шаблона карты (tmx) в игру. --1. Переработан класс Room. 2. Переработано метод move в PLayer (работает некорректно: дергано, при дробной скорости не двигается).

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import copy
 import os
 import random
 import sys
 
 import pygame
+import pytmx
 import const
 
 
@@ -47,66 +49,15 @@ class SpritesGroup(pygame.sprite.Group):
                 sprite.update(*args, **kwargs)
 
 
-class Block(pygame.sprite.Sprite):
-    def __init__(
-            self,
-            row: int,
-            col: int,
-            image: pygame.Surface,
-            type_block: str,
-            *groups
-    ):
-        super().__init__(*groups)
-        self.row, self.col = row, col
-        self.rect = pygame.Rect(
-            *map(
-                lambda coord: coord * const.SIZE_BLOCK, (col, row)
-            ),
-            const.SIZE_BLOCK, const.SIZE_BLOCK
-        )
-        self.image = pygame.transform.smoothscale(image, self.rect.size)
-        self.type_block = type_block
-
-    def __repr__(self):
-        return f'Block({self.row}, {self.col}) -> {self.type_block=}, {self.image=}'
-
-
 class Room:
     def __init__(
             self,
-            count_rows_area: int,
-            count_cols_area: int
+            filename_map: str
     ):
-        self.count_cols = count_cols_area + 2
-        self.count_rows = count_rows_area + 2
-        self.group_room = SpritesGroup()
-        GroupContainer().add_group(self, self.group_room)
-        self.width = self.count_cols * const.SIZE_BLOCK
-        self.height = self.count_rows * const.SIZE_BLOCK
-        wall = load_image('brick.png', -1)
-        void = load_image('cobblestone.png', -1)
-        self.matrix = list(
-            map(
-                lambda i: list(
-                    map(
-                        lambda j: Block(
-                            i, j, *(wall, const.TYPE_BLOCK_WALL)
-                            if i in {0, self.count_rows - 1} or
-                            j in {0, self.count_cols - 1} else
-                            (void, const.TYPE_BLOCK_VOID),
-                            self.group_room
-                        ),
-                        range(self.count_cols)
-                    )
-                ),
-                range(self.count_rows)
-            )
-        )
+        self.__map = pytmx.load_pygame(filename_map)
 
-    def set_block(self, block: Block):
-        row, col = block.row, block.col
-        if row in range(self.count_rows) and col in range(self.count_cols):
-            self.matrix[row][col] = block
+    def convert_coord(self, x: float | int, y: float | int):
+        return y // self.__map.tileheight, x // self.__map.tilewidth
 
     def check_correct_x_y(self, x: float | int, y: float | int):
         row, col = x // const.SIZE_BLOCK, y // const.SIZE_BLOCK
@@ -114,20 +65,65 @@ class Room:
             return True
         return False
 
-    def draw(self, screen):
-        self.group_room.draw(screen)
+    def draw(self, screen: pygame.Surface):
+        for row in range(self.__map.height):
+            for col in range(self.__map.width):
+                image = self.__map.get_tile_image(row, col, 0)
+                if image is not None:
+                    screen.blit(
+                        image,
+                        (
+                            row * self.__map.tilewidth,
+                            col * self.__map.tilewidth
+                        )
+                    )
 
     def check_correct_row_col(self, row: int, col: int):
-        if row in range(self.count_rows) and col in range(self.count_cols):
+        if row in range(self.__map.height) and col in range(self.__map.height):
             return True
         return False
 
-    def get_block_by_row_col(self, row: int, col: int):
-        return self.matrix[row][col]
+    def get_tile_range_connection_blocks(
+            self,
+            point1,
+            point2,
+            type_tile: str
+    ):
+        """Возвращает первую попавшеюся клетку, которая находиться в
+        диапазоне 2-х точек и имеет type_tile."""
+        if point1[0] == point2[0]:
+            row = point1[0]
+            step = 1 if point1[1] <= point2[1] else -1
+            for i in range(point1[1], point2[1] + step, step):
+                tile = self.get_tile_properties(row, i)
+                if tile is not None and tile['type'] == type_tile:
+                    return self.get_tile_rect(row, i)
+        elif point1[1] == point2[1]:
+            col = point1[1]
+            step = 1 if point1[0] <= point2[0] else -1
+            for i in range(point1[0], point2[0] + step, step):
+                tile = self.get_tile_properties(i, col)
+                if tile is not None and tile['type'] == type_tile:
+                    return self.get_tile_rect(i, col)
+        else:
+            raise ValueError('Одна из координат должна быть одинаковой.')
 
-    def get_block_by_x_y(self, x: float | int, y: float | int):
-        row, col = x // const.SIZE_BLOCK, y // const.SIZE_BLOCK
-        return self.matrix[row][col]
+    def get_tile_properties(self, row: int, col: int, layer=0) -> dict | None:
+        """Возвращает словарь свойств клетки."""
+        try:
+            result = self.__map.get_tile_properties(col, row, layer)
+        except Exception:
+            result = None
+        return result
+
+    def get_tile_rect(self, row: int, col: int):
+        return pygame.Rect(
+            (col * self.__map.tilewidth, row * self.__map.tileheight),
+            (self.__map.width, self.__map.height)
+        )
+
+    def get_map(self):
+        return pytmx.load_pygame(self.__map.filename)
 
 
 class Level:
@@ -172,8 +168,9 @@ class Level:
         row_current_room += k_row
         col_current_room += k_col
         if (
-            self.check_correction_coord(row_current_room, col_current_room) and
-            self.get_room(row_current_room, col_current_room) is not None
+                self.check_correction_coord(row_current_room,
+                                            col_current_room) and
+                self.get_room(row_current_room, col_current_room) is not None
         ):
             self.coord_current_room = (row_current_room, col_current_room)
             return True
@@ -194,12 +191,10 @@ class Entity(pygame.sprite.Sprite):
     ):
         super().__init__(*groups)
         self.angle_view = 0
-        self.speed = speed_move
+        self.speed = speed_move / const.FPS
         self.name = name
         self.hp = hp
         self.size_entity = size_entity
-        if not (self.size_entity % const.SIZE_BLOCK):
-            self.size_entity -= 2
         self.rect = pygame.Rect(
             *map(lambda coord: coord - size_entity / 2, (x, y)),
             self.size_entity, self.size_entity
@@ -236,90 +231,89 @@ class Player(Entity):
     def update(self, *args, **kwargs):
         self.move(*args, **kwargs)
 
-    def move(self, room: Room, direction: tuple[int, int]):
+    def move(
+            self,
+            room: Room,
+            direction: tuple[int | float, int | float]
+    ):
         """Перемещение игрока."""
-        # новый хитбокс
         x_direction, y_direction = direction
+        # Сдвиг координат.
         k_x = x_direction * self.speed
         k_y = y_direction * self.speed
-        new_hitbox = self.rect.move(k_x, k_y)
-        borders_current_rect = {
-            (0, -1): self.rect.top,
-            (0, 1): self.rect.bottom,
-            (-1, 0): self.rect.left,
-            (1, 0): self.rect.right
-        }
-        borders_new_rect = {
-            (0, -1): new_hitbox.top,
-            (0, 1): new_hitbox.bottom,
-            (-1, 0): new_hitbox.left,
-            (1, 0): new_hitbox.right
-        }
-        border_current_rect_towards = borders_current_rect[direction]
-        border_new_rect_towards = borders_new_rect[direction]
-        if border_new_rect_towards < 0:
-            border_new_rect_towards = 0
-        elif border_new_rect_towards >= (
-                border_room := room.width if direction[0] else room.height
-        ):
-            border_new_rect_towards = border_room - 1
-        border_left = borders_current_rect[tuple(reversed(direction))]
-        border_right = borders_current_rect[
-            tuple(map(lambda x: -x, reversed(direction)))
-        ]
-        flag_break = False
-        block = None
-        begin_current = border_current_rect_towards // const.SIZE_BLOCK
-        end_new = border_new_rect_towards // const.SIZE_BLOCK
-        step_border_towards = 1 if end_new >= begin_current else -1
-        begin_left = border_left // const.SIZE_BLOCK
-        end_right = border_right // const.SIZE_BLOCK
-        step_borders = 1 if end_right >= begin_left else -1
-        for i in range(
-                begin_current,
-                end_new + step_border_towards,
-                step_border_towards
-        ):
-
-            for j in range(
-                    begin_left,
-                    end_right + step_borders,
-                    step_borders
-            ):
-                coord = (i, j) if direction[1] else (j, i)
-                if room.get_block_by_row_col(
-                        *coord).type_block == const.TYPE_BLOCK_WALL:
-                    block = room.get_block_by_row_col(*coord)
-                    flag_break = True
-                    break
-            if flag_break:
-                break
-        if block is not None:
-            borders_block = {
-                (0, -1): block.rect.top,
-                (0, 1): block.rect.bottom,
-                (-1, 0): block.rect.left,
-                (1, 0): block.rect.right
-            }
-            border_block = borders_block[
-                tuple(map(lambda x: -x, direction))
-            ]
-            distance = border_block - border_current_rect_towards - (
-                direction[0] if direction[0] else direction[1]
+        # Перебираем стороны по направлению (+row -> правая сторона)
+        for i, line in enumerate(self.__get_coord_line_rect_by_direction(
+                self.rect, direction
+        )):
+            if line is None:
+                continue
+            # Получаем клетку, которая является стеной.
+            connection_tile = room.get_tile_range_connection_blocks(
+                *map(
+                    lambda coord: room.convert_coord(*coord),
+                    line
+                ),
+                const.TYPE_BLOCK_WALL
             )
-            if direction[0]:
-                k_x = distance
-            elif direction[1]:
-                k_y = distance
+            if connection_tile is not None:
+                # Получаем противоположную по взгляду сторону (клетки)
+                opposite_sides_direction = \
+                    self.__get_coord_line_rect_by_direction(
+                        connection_tile,
+                        tuple(map(lambda x: -x, direction))
+                    )
+                if not i:
+                    x2 = line[0][0]
+                    x1 = opposite_sides_direction[0][0][0]
+                    k_x = x1 - x2
+                else:
+                    y2 = line[1][1]
+                    y1 = opposite_sides_direction[1][1][1]
+                    k_y = y1 - y2
         self.rect.move_ip(k_x, k_y)
+
+    @staticmethod
+    def __get_coord_line_rect_by_direction(
+            rect: pygame.Rect,
+            direction: tuple[int | float, int | float]
+    ):
+        result = []
+        if direction[0]:
+            if direction[0] > 0:
+                line = (
+                    rect.topright,
+                    rect.bottomright
+                )
+            else:
+                line = (
+                    rect.topleft,
+                    rect.bottomleft
+                )
+        else:
+            line = None
+        result.append(line)
+        if direction[1]:
+            if direction[1] > 0:
+                line = (
+                    rect.bottomleft,
+                    rect.bottomright
+                )
+            else:
+                line = (
+                    rect.topleft,
+                    rect.topright
+                )
+        else:
+            line = None
+        result.append(line)
+        return result
 
 
 def load_image(name: str, colorkey=None):
     fullname = os.path.join('data', name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
+        raise FileNotFoundError(f"Файл с изображением '{fullname}' не найден")
     image = pygame.image.load(fullname)
     if colorkey is not None:
         image = image.convert()
@@ -334,69 +328,36 @@ def load_image(name: str, colorkey=None):
 def main():
     pygame.init()
     main_screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    container = GroupContainer()
-    level = Level(3, 3)
-    for i in range(3):
-        for j in range(3):
-            room = Room(random.randint(5, 10), random.randint(5, 10))
-            level.set_room(room, i, j)
-    level.set_current_room(0, 0)
+    player_screen = pygame.Surface(main_screen.get_size())
+    room = Room('data/templates_maps/test_map.tmx')
+    group = SpritesGroup()
+    player = Player(
+        510, 500, 30, load_image('hero.png', -1), 60, 0, '12', group
+    )
     clock = pygame.time.Clock()
     running = True
-    speed_player = {
-        'UP': (0, -1),
-        'DOWN': (0, 1),
-        'LEFT': (-1, 0),
-        'RIGHT': (1, 0)
-
-    }
-    image_hero = load_image('hero.png', -1)
-    player_group = SpritesGroup()
-    player = Player(
-        250, 250, const.SIZE_PLAYER, image_hero, const.SPEED_PLAYER,
-        100, 'qwerty', player_group
-    )
-    container.add_group(player, player_group)
-    data = {
-        pygame.K_w: (-1, 0),
-        pygame.K_a: (0, -1),
-        pygame.K_s: (1, 0),
-        pygame.K_d: (0, 1),
-    }
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (
-                event.type == pygame.KEYDOWN and
-                event.key == pygame.K_ESCAPE
+                    event.type == pygame.KEYDOWN and
+                    event.key == pygame.K_ESCAPE
             ):
                 running = False
-            if event.type == pygame.KEYDOWN:
-                button = event.key
-                if button in data:
-                    if level.displace_current_room(*data[button]):
-                        player.set_position(const.SIZE_BLOCK / 2 * 3,
-                                            const.SIZE_BLOCK / 2 * 3)
         keys = pygame.key.get_pressed()
-        room = level.get_current_room()
-        if keys[pygame.K_UP]:
-            player_group.update(room, speed_player['UP'])
-        if keys[pygame.K_DOWN]:
-            player_group.update(room, speed_player['DOWN'])
-        if keys[pygame.K_LEFT]:
-            player_group.update(room, speed_player['LEFT'])
-        if keys[pygame.K_RIGHT]:
-            player_group.update(room, speed_player['RIGHT'])
-        current_room_blocks = level.get_current_room()
-        room_screen = pygame.Surface((current_room_blocks.width,
-                                      current_room_blocks.height))
+        if keys[pygame.K_w]:
+            player.move(room, (0, -1))
+        if keys[pygame.K_a]:
+            player.move(room, (-1, 0))
+        if keys[pygame.K_s]:
+            player.move(room, (0, 1))
+        if keys[pygame.K_d]:
+            player.move(room, (1, 0))
         main_screen.fill('black')
-        room_screen.fill('black')
-        container.draw_group(room_screen, current_room_blocks)
-        player_group.draw(room_screen)
-        main_screen.blit(
-            room_screen, (0, 0),
-            player.get_screen_player(*main_screen.get_size())
-        )
+        player_screen.fill('black')
+        room.draw(player_screen)
+        group.draw(player_screen)
+        main_screen.blit(player_screen, (0, 0),
+                         player.get_screen_player(*player_screen.get_size()))
         pygame.display.flip()
         clock.tick(const.FPS)
     pygame.quit()
